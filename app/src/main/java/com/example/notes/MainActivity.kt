85161fdc7f33726b79686aeaab452af3e42392c1
Susinidu4 <155086679+Susinidu4@users.noreply.github.com>
2024-05-14 15:24:20 +0530

    Create update and delete function

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -6,19 +6,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private  lateinit var db: NotesDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = NotesDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(),this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
         binding.addButton.setOnClickListener{
             val intent = Intent(this, AddNoteActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        notesAdapter.refreshData(db.getAllNotes())
     }
 }
--- app/src/main/java/com/example/notes/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/notes/NotesDatabaseHelper.kt
@@ -36,4 +36,63 @@ class NotesDatabaseHelper(context:Context) : SQLiteOpenHelper(context, DATABASE_
         db.insert(TABLE_NAME, null,values)
         db.close()
     }
+
+    //display
+    fun getAllNotes(): List<Note>{
+        val notesList = mutableListOf<Note>()
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(query, null)
+
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+            val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+            val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+            val note = Note(id,title, content)
+            notesList.add(note)
+        }
+        cursor.close()
+        db.close()
+        return notesList
+    }
+
+    //update
+    fun updateNote(note: Note){
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_TITLE, note.title)
+            put(COLUMN_CONTENT, note.content)
+        }
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(note.id.toString())
+        db.update(TABLE_NAME, values, whereClause, whereArgs)
+        db.close()
+    }
+
+    //after update display
+    fun getNoteByID(noteId: Int): Note{
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID = $noteId"
+        val cursor = db.rawQuery(query, null)
+        cursor.moveToFirst()
+
+        val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+        val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+        val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+        cursor.close()
+        db.close()
+        return Note(id, title, content)
+    }
+
+    //delete
+    fun deleteNote(noteId: Int){
+        val db = writableDatabase
+        val whereClause ="$COLUMN_ID = ?"
+        val whereArgs = arrayOf(noteId.toString())
+
+        db.delete(TABLE_NAME, whereClause, whereArgs)
+        db.close()
+    }
 }
